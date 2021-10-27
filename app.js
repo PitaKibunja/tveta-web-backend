baed053343df926595a7f3943d0d6af353e66ef6
MAIN-ACC <pitakibunja@gmail.com>
2021-10-27 15:22:37 +0300

    backend now communicating with the front end effectively

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require('express')
-const mongoose=require('mongoose')
+const mongoose = require('mongoose')
+const cors = require('cors')
+
 const app = express()
 require('dotenv/config')
 
 //import routes
 const postRoute = require('./routes/board')
 
+//use cors to allow access of data from the front-end
+app.use(cors({
+    origin:'*'
+}))
+
 app.use(express.json());
-app.use('/posts', postRoute)
+app.use('/api_v_1/board', postRoute)
 //connect to db
 mongoose.connect(
     process.env.DB_CONNECTION,
